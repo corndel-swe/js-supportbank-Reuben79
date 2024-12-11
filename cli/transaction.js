665c502509640a001a708ec74f4c614f83c7e068
reuben longley <reuben.longley@clarionhg.com>
2024-12-11 14:20:19 +0000

    cli/index.js

--- cli/transaction.js
+++ cli/transaction.js
@@ -6,7 +6,7 @@ transactionController
   .command('log <from> <to> <amount>')
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
-    // TODO
+    console.log(`Transaction from ${from} to ${to} for amount ${amount}`)
   })
 
 export default transactionController
--- exercises/hello.js
+++ exercises/hello.js
@@ -3,3 +3,8 @@ const num = 3495
 function sayHi() {
   return 'Hello ES6 Modules!'
 }
+
+export { num,}
+
+export default sayHi
+
