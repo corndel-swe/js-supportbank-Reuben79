32e47e046552c25200a1499b803ae691cf074ffa
reuben longley <reuben.longley@clarionhg.com>
2024-12-17 14:21:56 +0000

    Workshop 3

---
+++ exercises/erates.js
@@ -0,0 +1,9 @@
+export async function getLatestExchangeRates(appID) {
+
+ const response = await fetch (`https://openexchangerates.org/api/latest.json?app_id=${appId}`) 
+
+ const data = await response.json()
+
+ return data.getLatestExchangeRates
+
+}
