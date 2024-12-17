ee8a5cb8f471ee746c34cc35325cf3103ed9f883
reuben longley <reuben.longley@clarionhg.com>
2024-12-17 14:09:32 +0000

    d3e3

--- exercises/pokeAPI.js
+++ exercises/pokeAPI.js
@@ -1,3 +1,10 @@
 export async function getPokemonList() {
   // TODO
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+
+  const data = await response.json()
+  console.log(data) 
+  
+  return data.results.map(pokemon => pokemon.name)
+
 }
